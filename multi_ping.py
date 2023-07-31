dd1d324dfa21a066a6a9a012515adecb7352c85e
Jurgen Strydom <jurgen.strydom@gmail.com>
2023-07-31 08:44:52 +0200

    the pings run in par, but still waiting for response before next loop

--- multi_ping.py
+++ multi_ping.py
@@ -1,74 +1,78 @@
 import typer
 import subprocess
 import asyncio
-import itertools as it
-import os
-import random
+import time
+import re
 import time
 
+async def extract_data_from_ping_response(response):
+    packets_received = re.search('r"([0-9]+)\s+packets transmitted.*([0-9]+)\s+packets received"', response, re.IGNORECASE)
+    return packets_received.group(2)
 
-async def ping(host):
-    t = time.perf_counter()
-    try:
-        response = subprocess.check_output(
-            ['ping', '-c', '1', f'{host}'],
-            universal_newlines=True,
-        )
-    except subprocess.CalledProcessError:
-        response = None
-    duration = time.perf_counter() - t
-    await asyncio.sleep(1 - duration)
+async def call_subroutine(host):
+    response, error = None, None
+    print(f'calling subroutine for {host}')
+    cmd = ' '.join(['ping', '-c', '1', f'{host}'])
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await proc.communicate()
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        response = stdout.decode()
+    if stderr:
+        error = stderr.decode()
     return response
 
-async def produce(name: int, ip: str, q: asyncio.Queue) -> None:
+async def ping(host):
+    print(f'pining {host}')
+    try:
+        response = await call_subroutine(host)
+        result = extract_data_from_ping_response(response)
+    except subprocess.CalledProcessError:
+        result = None
+    print(result)
+    return result
+
+async def ping_multiple(ips: list[str]):
+    print('ping_multiple')
     while True:
-        r = await ping(ip)
-        t = time.perf_counter()
-        await q.put((r, t))
-        print(f"Producer {name} added <{ip}> to queue.")
+        print('cadence loop')
+        for p in asyncio.as_completed([ping(ip) for ip in ips]):
+            print('ping loop')
+            result = await p
+        print('cadence loop done, sleeping')
+        await asyncio.sleep(1)
 
 
-async def consume(name: int, q: asyncio.Queue) -> None:
-    while True:
-        ip, t = await q.get()
-        now = time.perf_counter()
-        print(f"Consumer {name} got element <{ip}>"
-              f" in {now-t:0.5f} seconds.")
-        q.task_done()
+def main(ips: list[str]):
+    print('main')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(ping_multiple(ips))
+    loop.close()
+
+# app = typer.Typer()
 
 
-async def main(ips: list[str]):
-    q = asyncio.Queue()
-    ncon = len(ips)
-    consumers = [asyncio.create_task(consume(n, q)) for n in range(ncon)]
-    producers = [asyncio.create_task(produce(n, ip, q)) for n, ip in enumerate(ips)]
-    await asyncio.gather(*producers)
-    await q.join()  # Implicitly awaits consumers, too
-    for c in consumers:
-        c.cancel()
+# @app.command()
+# def hello(name: str):
+#     print(f"Hello {name}")
 
 
-app = typer.Typer()
-
-
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
-
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+# @app.command()
+# def goodbye(name: str, formal: bool = False):
+#     if formal:
+#         print(f"Goodbye Ms. {name}. Have a good day.")
+#     else:
+#         print(f"Bye {name}!")
 
 
 if __name__ == "__main__":
     # app(prog_name="mping")
-    args = ['1.1.1.1', '8.8.8.8', '192.168.0.1']
+    args = ['1.1.1.1', '8.8.8.8', '192.168.0.1', '1.2.3.4']
     start = time.perf_counter()
-    asyncio.run(main(args))
+    main(args)
     elapsed = time.perf_counter() - start
     print(f"Program completed in {elapsed:0.5f} seconds.")
     
